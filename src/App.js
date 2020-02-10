14d25efe1962791947fcc66a7dac79aa84e543eb
Will Holcomb <will@dhappy.org>
2020-02-09 22:10:42 -0500

    workin' on unzipping epubs ⛏️

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import './App.css'
+import * as Z from './Unzip'
+import * as ZExt from './Unzip/Ext'
+const zip = {...Z.zip, ...ZExt.zip}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default (props) => {
+  useEffect(() => {
+    fetch('pg34488-images.epub')
+      .then(async (res) => {
+        console.log(res, zip)
+        const reader = res.body.getReader()
+        zip.createReader(reader, function(zipReader) {
+					zipReader.getEntries(console.info);
+        }, console.error)
+      })
+    }, [])
+
+  return <React.Fragment>
+    <head>
+      <link rel="icon" href="favicon.ico" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="theme-color" content="#000000" />
+      <link rel="apple-touch-icon" href="logo192.png" />
+      <link rel="manifest" href="manifest.json" />
+      <title>Μïmir</title>
+    </head>
+    <body>
+    </body>
+  </React.Fragment>
 }
-
-export default App;
